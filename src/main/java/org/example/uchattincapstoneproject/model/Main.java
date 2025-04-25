11a2279a18f3aba9cfde578baac8bc206dcb35db
Nadu628 <eugen5@farmingdale.edu>
2025-04-25 17:28:28 -0400

    fixed all fxml files

--- src/main/java/org/example/uchattincapstoneproject/model/Main.java
+++ src/main/java/org/example/uchattincapstoneproject/model/Main.java
@@ -10,6 +10,7 @@ import com.microsoft.cognitiveservices.speech.ResultReason;
 import com.microsoft.cognitiveservices.speech.SpeechSynthesisResult;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -19,13 +20,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource(
-                "main-view.fxml"));
-
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        try{
+            System.out.println("attempting to start login screen");
+            FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("/views/login.fxml"));
+            Parent login = loginLoader.load();
+            System.out.println("login.fxml load successful");
+            Scene scene = new Scene(login, 800, 600);
+            stage.setScene(scene);
+            stage.setTitle("Login");
+            stage.show();
+            System.out.println("log in screen displayed");
+        }catch (Exception e){
+            System.out.println("Error initializing log in screen");
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
