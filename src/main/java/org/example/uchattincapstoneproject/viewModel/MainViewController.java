d9087184e4d1bb04e3e7ecf502297c8d62d47fa9
Nadu628 <eugen5@farmingdale.edu>
2025-04-30 17:05:00 -0400

    separated log in fxml for easier coding

--- src/main/java/org/example/uchattincapstoneproject/viewModel/MainViewController.java
+++ src/main/java/org/example/uchattincapstoneproject/viewModel/MainViewController.java
@@ -1,4 +1,14 @@
 package org.example.uchattincapstoneproject.viewModel;
 
+import javafx.scene.control.Label;
+
 public class MainViewController {
+    private int userID;
+    public void setUsername(String username) {
+        Label welcomeLabel = new Label("Welcome " + username + "!");
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
 }
